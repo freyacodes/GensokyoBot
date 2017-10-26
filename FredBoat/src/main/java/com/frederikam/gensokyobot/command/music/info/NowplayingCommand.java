0a0f6957cfa9544eef815e1649fcd7b8373d41a1
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2017-10-26 12:30:32 +0200

    Fix GR logo

--- FredBoat/src/main/java/com/frederikam/gensokyobot/command/music/info/NowplayingCommand.java
+++ FredBoat/src/main/java/com/frederikam/gensokyobot/command/music/info/NowplayingCommand.java
@@ -62,7 +62,7 @@ public class NowplayingCommand extends Command implements IMusicCommand {
                 MessageFormat.format(I18n.get(channel.getGuild()).getString("npRatingRange"), data.getJSONObject("MISC").getInt("RATING"), data.getJSONObject("MISC").getInt("TIMESRATED"));
 
         String albumArt = data.getJSONObject("MISC").getString("ALBUMART").equals("") ?
-                "https://i.imgur.com/a1SQKuw.png" :
+                "https://i.imgur.com/diwlC.png" :
                 "https://gensokyoradio.net/images/albums/original/" + data.getJSONObject("MISC").getString("ALBUMART");
 
         String titleUrl = data.getJSONObject("MISC").getString("CIRCLELINK").equals("") ?

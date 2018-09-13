abd7c6435756bb3566accfacc3e14c5a6fb0860f
SixAiy <38549893+SixAiy@users.noreply.github.com>
2018-09-14 00:35:14 +0800

    Update CommandInitializer.java

--- FredBoat/src/main/java/com/frederikam/gensokyobot/commandmeta/init/CommandInitializer.java
+++ FredBoat/src/main/java/com/frederikam/gensokyobot/commandmeta/init/CommandInitializer.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Frederik Ar. Mikkelsen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 package com.frederikam.gensokyobot.commandmeta.init;
 
 import com.frederikam.gensokyobot.command.admin.BotRestartCommand;
@@ -12,10 +36,9 @@ import com.frederikam.gensokyobot.command.maintenance.VersionCommand;
 import com.frederikam.gensokyobot.command.music.control.JoinCommand;
 import com.frederikam.gensokyobot.command.music.control.LeaveCommand;
 import com.frederikam.gensokyobot.command.music.info.NowplayingCommand;
-//import com.frederikam.gensokyobot.command.util.HelloCommand;
+import com.frederikam.gensokyobot.command.util.HelloCommand;
 import com.frederikam.gensokyobot.command.util.HelpCommand;
 import com.frederikam.gensokyobot.command.util.InviteCommand;
-//import com.frederikam.gensokyobot.command.util.PingCommand;
 import com.frederikam.gensokyobot.commandmeta.CommandRegistry;
 
 public class CommandInitializer {
@@ -32,8 +55,7 @@ public class CommandInitializer {
         CommandRegistry.registerCommand("shards", new ShardsCommand());
         CommandRegistry.registerCommand("invite", new InviteCommand());
         CommandRegistry.registerCommand("version", new VersionCommand());
-        //CommandRegistry.registerCommand("ping", new PingCommand());
-        //CommandRegistry.registerCommand("hello", new HelloCommand());
+        CommandRegistry.registerCommand("hello", new HelloCommand());
         CommandRegistry.registerCommand("np", new NowplayingCommand());
         CommandRegistry.registerAlias("np", "nowplaying");
 
